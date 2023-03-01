28e1f0ab5acaec96bacd38fad3a497396da8a781
Aifuagbon <presh8042@gmail.com>
2023-03-01 03:49:04 -0800

    good

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * print_triangle - create triangle
- * @size: params
- * Return: 0
- */
+* print_triangle - create triangle
+* @size: params
+* Return: 0
+*/
 
 void print_triangle(int size)
 {
-	int i = 0, j, n = size - 1;
-	if (size > 0)
+int i = 0, j, n = size - 1;
+if (size > 0)
 {
 for (; i < size; i++)
 {
-	for (j = 0; j < size; j++)
+for (j = 0; j < size; j++)
 {
-	if (j < n)
+if (j < n)
 _putchar(' ');
 else
 _putchar('#');
