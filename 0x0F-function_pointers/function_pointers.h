4fd7b02350af5d3f92a8ee1cff04730f1eacd0dc
Aifuagbon <presh8042@gmail.com>
2023-03-22 12:09:32 -0700

    good

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,52 +1,12 @@
-#include <stdio.h>
-#include "function_pointers.h"
-
+	#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
 /**
- * print_name_as_is - prints a name as is
- * @name: name of the person
+ * File: main
  *
- * Return: Nothing.
- */
-void print_name_as_is(char *name)
-{
-    printf("Hello, my name is %s\n", name);
-}
+ * Desc: header file
+**/
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
-/**
- * print_name_uppercase - print a name in uppercase
- * @name: name of the person
- *
- * Return: Nothing.
- */
-void print_name_uppercase(char *name)
-{
-    unsigned int i;
-
-    printf("Hello, my uppercase name is ");
-    i = 0;
-    while (name[i])
-    {
-        if (name[i] >= 'a' && name[i] <= 'z')
-        {
-            putchar(name[i] + 'A' - 'a');
-        }
-        else
-        {
-            putchar(name[i]);
-        }
-        i++;
-    }
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_name("Bob", print_name_as_is);
-    print_name("Bob Dylan", print_name_uppercase);
-    printf("\n");
-    return (0);
-}
+#endif
