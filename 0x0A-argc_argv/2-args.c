6e3047d7a01975c81148eb430da2618c26c4ea61
Aifuagbon <presh8042@gmail.com>
2023-06-13 11:03:12 -0700

    good

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,8 +2,12 @@
 #include "main.h"
 
  /**
-  * main - prints all arguments it receives.
-  */
+ * main - prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 
 
 int main(int argc __attribute__((unused)), char *argv[])
