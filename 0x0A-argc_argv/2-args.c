b8d6146faa7849125dc15fc5e93b66bb97cebd39
Aifuagbon <presh8042@gmail.com>
2023-06-13 10:45:11 -0700

    okay

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,6 +2,9 @@
 
 int main(int argc __attribute__((unused)), char *argv[])
 {
+	/**
+	 * write a program that prints all arguments it receives.
+	 */
 
 printf("%s\n", argv[0]);
 printf("%s\n", argv[1]);
