efed5d84dc17fbf8f940a2a42423f057bbc2eab0
Aifuagbon <presh8042@gmail.com>
2023-06-13 08:24:58 -0700

    okay

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]){
+int main(int argc __attribute__((unused)), char *argv[]){
 
 printf("%s\n", argv[0]);
-printf("%s\n", argv[1});
+printf("%s\n", argv[1]);
 printf ("%d\n", argc);
 
 return (0);
