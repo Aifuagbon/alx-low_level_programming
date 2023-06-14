faca3d355f6f4f1bc6fb9716399c74eecbaa1ad3
Aifuagbon <presh8042@gmail.com>
2023-06-13 19:50:42 -0700

    good

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Multiplies two numbers.
+ * @argc: The number of argument supplied to the program.
+ * @argv: An array of pointers to the program.
+ *
+ * return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+
+(argc == 2);
+printf("%d x %d\n", argc, argc);
+return (0);
+} 
