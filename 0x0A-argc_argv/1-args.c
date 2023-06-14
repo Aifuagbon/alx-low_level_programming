be81901794e36a301e62f12d0d011ef4f4eb67f9
Aifuagbon <presh8042@gmail.com>
2023-06-13 18:57:48 -0700

    okay

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[] __attribute__ ((unused))){
-	
+int main(int argc, char *argv[] __attribute__ ((unused))) {
 	
 	printf("%d\n", argc - 1);
 
-	return(0);
+	return (0);
 }
 	
 
