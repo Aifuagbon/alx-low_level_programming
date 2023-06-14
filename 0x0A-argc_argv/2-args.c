cb7391e00a53e08ef1ce4f001776f22fe3272299
Aifuagbon <presh8042@gmail.com>
2023-06-13 18:38:20 -0700

    good

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include "main.h"
 
  /**
-  * main - prints all arguments it receives
-  * @argc: number of argumentsi
-  * @argv: array of arguments
-  * Return: Always 0 (Success)
+  * main - Prints all arguments it receives.
+  * @argc: The number of arguments supplied to the program.
+  * @argv: An array of pointers to the arguments. 
+  *
+  * Return: Always 0.
   */
 int main(int argc __attribute__((unused)), char *argv[])
 {
