5b1341c1f51cbdf50c523dd8f91c1dc3526f5e14
Aifuagbon <presh8042@gmail.com>
2023-06-14 00:00:06 -0700

    okay

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,16 +9,17 @@
  * return: Always 0.
  */
 int main(int argc, char *argv[]) {
+	int num1 num2 product
     if (argc != 3) {
         printf("Error\n");
         return 1;
     }
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[2]);
-    result = num1 * num2;
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
+    int product = num1 * num2;
 
-    printf("%d\n", result);
+    printf("%d\n", product);
 
     return 0;
 }
